791190c4a9f9a8bc919e0598693ebda82f67e3f5
Luis <luismayta08@gmail.com>
2023-05-14 18:45:04 -0500

    actualizando html para incluir componentes

---
+++ components/header.php
@@ -0,0 +1,28 @@
+<header class="header" id="header">
+    <nav class="nav f-elements f-elements--between" id="header-nav">
+        <div class="header__container-logo">
+            <a href="index.html">
+                <img src="./assets/img/webp/logoBlanco.webp" alt="logo" class="nav__logo">
+            </a>
+        </div>
+        <div class="container f-elements f-elements--between p-relative">
+            <a href="#header-nav" class="nav__icon nav__icon--menu">
+                <i class="bi bi-list"></i>
+                </a>
+                <a href="#header-close" class="nav__icon nav__icon--close">
+                <i class="bi bi-x-circle"></i>
+                </a>
+            <ul class="list list--header f-elements f-col gap-md f-elements--center md:f-row">
+                <li>
+                    <a href="#services" class="list__link">Servicios</a>
+                </li>
+                <li>
+                    <a href="#projects" class="list__link">Portafolio</a>
+                </li>
+                <li>
+                    <a href="#contact" class="list__link">Contacto</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+</header>

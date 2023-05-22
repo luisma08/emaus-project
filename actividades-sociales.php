d0788d228e8b7520c977a5b822b047bf55229b76
Luis <luismayta08@gmail.com>
2023-05-22 18:40:49 -0500

    actualizando contenido de pagina actividades sociales

--- actividades-sociales.php
+++ actividades-sociales.php
@@ -1,7 +1,52 @@
-<section class="section" id="projects">
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./styles/css/style.css">
+    <link rel="stylesheet" href="./styles/css/fe.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.2/font/bootstrap-icons.css">
+    <link
+      rel="stylesheet"
+      href="https://cdn.jsdelivr.net/npm/swiper/swiper-bundle.min.css"
+    />
+
+    <!-- FONT FAMILY -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
+
+    <!-- ICON -->
+    <link rel="shortcut icon" href="assets/icon/favicon.webp" type="image/svg+xml">
+
+    <!-- HTML Meta Tags -->
+    <title>Actividades Sociales</title>
+    <meta name="description" content="Traperos de Emaús - Manos Solidarias">
+    <meta name="keywords" content="">
+</head>
+<body class="body">
+
+    <!-- Agregando componente header -->
+    <?php include('components/header.php') ?>
+    <!-- FIN -->
+
+    <main class="main">
+        <section class="banner">
+            <div class="container f-elements f-elements--responsive gap-sm">
+                <div class="banner__container">
+                    <h1 class="banner__title">Traperos de Emaús <span class="banner__title-span">Donaciones</span></h1>
+                    <h2 class="banner__subtitle">¡Dona desde la comodidad de tu hogar!</h2>
+                    <p class="banner__description">
+                        Todo lo útil que ya no necesites nos permitirá mensualmente ayudar a los más necesitados
+                    </p>
+                    <a class="button button--primary" href="#contact"><i class="bi bi-balloon-heart"></i> Ayudanos a Donar</a>
+                </div>
+            </div>
+        </section>
+        <section class="section" id="projects">
             <div class="container f-elements f-col gap-md">
-              <h2 class="section__title">Tu donacion se convierte en Actividades Sociales</h2>
-              <div class="f-elements f-col f-elements--center">
+                <h2 class="section__title">Tu donacion se convierte en Actividades Sociales</h2>
+                <div class="f-elements f-col f-elements--center">
                     <div class="swiper mySwiper4"">
                         <div class="swiper-wrapper">
                             <div class="swiper-slide">
@@ -109,3 +154,13 @@
                 </div>
             </div>
         </section>
+    </main>
+
+    <!-- Agregando componente footer -->
+    <?php include('components/footer.php') ?>
+    <!-- FIN -->
+
+    <script src="./js/swiper.js" type="module"></script>
+    <script src="./js/header.js"></script>
+</body>
+</html>
